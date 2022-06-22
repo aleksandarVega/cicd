da704e12aaf9b2976d0d9fefaf66cada418ade3d
Aleksandar Vasic <a.vasic@vegait.rs>
2022-06-22 08:58:52 +0200

    Added np

--- src/components/YellowText.test.tsx
+++ src/components/YellowText.test.tsx
@@ -5,5 +5,5 @@ import {render} from '@testing-library/react-native';
 test('test the `<YellowText/>` value', () => {
   const component = render(<YellowText />);
   const textField = component.getByTestId('text');
-  expect(textField.props.children).toMatch('Hey dudes!');
+  expect(textField.props.children).toMatch('Hey dude!');
 });
