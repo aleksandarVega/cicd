0896be42de045973d0bc4bcb6f0702670b09a26a
Aleksandar Vasic <a.vasic@vegait.rs>
2022-06-29 12:35:46 +0200

    Dude

--- ios/CiCdGa/AppDelegate.h
+++ ios/CiCdGa/AppDelegate.h
@@ -1,8 +1,4 @@
-#if __has_include(<React/RCTBridgeDelegate.h>)
 #import <React/RCTBridgeDelegate.h>
-#else
-#import "RCTBridgeDelegate.h"
-#endif
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
