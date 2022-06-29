5115d97cef641542544a6fd0bbcf0d4a1de6d9f8
Aleksandar Vasic <a.vasic@vegait.rs>
2022-06-29 14:42:27 +0200

    Dude

--- ios/CiCdGa/AppDelegate.h
+++ ios/CiCdGa/AppDelegate.h
@@ -1,6 +1,6 @@
-
-#import <UIKit/UIKit.h>
 #import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
